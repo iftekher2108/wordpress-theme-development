5d9bcd94d02cb138fed011eb93a6cd9db8583782
iftekher2108 <parvezkhan21082002@gmail.com>
2024-10-22 18:26:27 +0600

    some important file added

--- functions.php
+++ functions.php
@@ -2,54 +2,67 @@
 
 // theme title
 add_theme_support('title-tag');
-add_theme_support( 	'custom-logo', ['height' => 350,'width'  => 600]);
+add_theme_support( 	'custom-logo', array('height' => 350,'width'  => 600));
 add_theme_support( 'widgets');
-// add_theme_support( 'custom-header');
-// add_theme_support( 'appearance-tools');
+add_theme_support( 'custom-header');
+add_theme_support( 'appearance-tools');
+add_theme_support('widgets-block-editor');
+add_theme_support('menus');
+$args = array(
+	'search-form',
+	'comment-form',
+	'comment-list',
+	'gallery',
+	'caption',
 
+	'meta',
+	'style',
+	'script',
+);
+add_theme_support( 'html5', $args );
 
 // wp-block added
+if( ! function_exists('theme_slug_setup')) {
 function theme_slug_setup() {
 	add_theme_support( 'wp-block-styles' );
 }
 add_action( 'after_setup_theme', 'theme_slug_setup' );
-// wp-block added
-
-// all css and javaScript file initialization
-function  iftekher2108_theme_all_css_js_file_initialization() {
-    
-    // theme main css initialization
-    wp_enqueue_style( 'iftekher2108-style',get_stylesheet_uri());
-
-    // css and plugins css file initialization
-
-    wp_register_style('bootstrap-cs',get_template_directory_uri().'/src/plugins/bootstrap/css/bootstrap-cs.min.css',[],'5.3.3');
-    wp_enqueue_style('bootstrap-cs');     // custom bootstrap initialization
-
-    wp_register_style( 'iftekher2108-main.min', get_template_directory_uri().'/src/scss/main.min.css',[],'1.0.0');
-    wp_enqueue_style('iftekher2108-main.min');      // custom css initialization
-
-
-    // javascript & js plugins file initialization
-
-    wp_register_script( 'bootstrap-cs',get_template_directory_uri(  ).'/src/plugins/bootstrap/js/bootstrap.bundle.min.js',[],'5.3.3', 'true');
-    wp_enqueue_script('bootstrap-cs');      // custom bootstrap initialization
-
-    wp_enqueue_script('jquery');        // jquery plugin initialization
- 
-    wp_register_script( 'iftekher2108-main', get_template_directory_uri().'/src/js/main.js',[],'1.0.0','true');
-    wp_enqueue_script('iftekher2108-main');         // custom js initialization
-
+// wp-block added    
 }
-add_action( 'wp_enqueue_scripts','iftekher2108_theme_all_css_js_file_initialization');  // file function initialization
-// all css and javaScript file initialization
 
+
+if( ! function_exists('iftekher_theme_css_style')) {
+// all css file initialization
+function iftekher_theme_css_style() {
+    // theme main css initialization
+    wp_enqueue_style( 'iftekher-style',get_stylesheet_uri());
+    // css and plugins css file initialization
+    wp_enqueue_style('bootstrap-cs',get_template_directory_uri().'/assets/plugins/bootstrap/css/bootstrap-cs.min.css',array(),'5.3.3');     // custom bootstrap initialization
+    wp_enqueue_style('iftekher-main.min', get_template_directory_uri().'/assets/scss/main.min.css',array(),'1.0.0');      // custom css initialization
+    // custom js initialization
+}
+add_action( 'wp_enqueue_scripts','iftekher_theme_css_style');  // file function initialization
+// all css file initialization   
+}
+
+if( ! function_exists('iftekher_theme_js_script')) {
+// all js file initialization
+function iftekher_theme_js_script() {
+        // javascript & js plugins file initialization
+        wp_enqueue_script('bootstrap-js',get_template_directory_uri(  ).'/assets/plugins/bootstrap/js/bootstrap.bundle.min.js',array(),'5.3.3', 'true');      // custom bootstrap initialization
+        wp_enqueue_script('jquery');        // jquery plugin initialization
+        wp_enqueue_script('iftekher-main',get_template_directory_uri().'/assets/js/main.js',array(),'1.0.0','true');  
+}
+add_action( 'wp_enqueue_scripts','iftekher_theme_js_script');  // file function initialization
+// all js file initialization
+}
 
 // register the menu items
-register_nav_menus( [   
-'main_menu' => __('main menu'),
-'footer_menu' => __('footer menu'),
-]);
+		register_nav_menus( array(
+	    	'primary_menu' => __( 'Primary Menu'),
+	    	'footer_menu'  => __( 'Footer Menu'),
+		) );
+
 // register the menu items
 
 
--- index.php
+++ index.php
@@ -8,19 +8,7 @@
 
 ?>
 
-<!DOCTYPE html>
-<html lang="<?php language_attributes(); ?>" class="no-js">
-
-<head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <title>
-        <?php
-        // bloginfo('title');
-        ?>
-</title> -->
-    <?php wp_head(); ?>
-</head>
+<?php get_header(); ?>
 
 <body <?php body_class(); ?>>
 
@@ -29,82 +17,25 @@
             <div class="container-fluid">
                 <a class="navbar-brand" href="#">Navbar</a>
 
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="<?php esc_attr_e('Toggle navigation', 'your-theme-slug'); ?>">
+                <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="">
                     <span class="navbar-toggler-icon"></span>
-                </button>
+                </button> -->
 
                 <?php
                 wp_nav_menu([
-				'theme_location'  => 'primary',              
-                'container'       => false,                   
-                'menu_class'      => 'navbar-nav ms-auto',
-				]);
+                    'theme_location' => 'primary',
+                    'container'      => false,
+                    'menu_class'     => 'navbar-nav ms-auto',
+                ]);
                 ?>
-						
-<?php
-// Add 'nav-item' class to each <li> element
-function add_nav_item_class($classes, $item, $args) {
-    $classes[] = 'nav-item';
-    return $classes;
-}
-add_filter('nav_menu_css_class', 'add_nav_item_class', 10, 3);
-
-// Add 'nav-link' class to each <a> element
-function add_nav_link_class($atts, $item, $args) {
-    $atts['class'] = 'nav-link';
-    return $atts;
-}
-add_filter('nav_menu_link_attributes', 'add_nav_link_class', 10, 3);
-
-// Add 'dropdown' class to parent <li> elements
-function add_dropdown_class($classes, $item, $args) {
-    if (in_array('menu-item-has-children', $classes)) {
-        $classes[] = 'dropdown';
-    }
-    return $classes;
-}
-add_filter('nav_menu_css_class', 'add_dropdown_class', 10, 3);
-
-// Add 'dropdown-toggle' class and attributes to parent <a> elements
-function add_dropdown_link_class($atts, $item, $args) {
-    if (in_array('menu-item-has-children', $item->classes)) {
-        $atts['class'] = 'nav-link dropdown-toggle';
-        $atts['data-bs-toggle'] = 'dropdown';
-        $atts['aria-haspopup'] = 'true';
-        $atts['aria-expanded'] = 'false';
-        $atts['role'] = 'button';
-    }
-    return $atts;
-}
-add_filter('nav_menu_link_attributes', 'add_dropdown_link_class', 10, 3);
-
-// Add 'dropdown-menu' class to submenu <ul> elements
-function add_dropdown_menu_class($classes, $args, $depth) {
-    if ($depth === 1) {
-        $classes[] = 'dropdown-menu';
-    }
-    return $classes;
-}
-add_filter('nav_menu_submenu_css_class', 'add_dropdown_menu_class', 10, 3);
-?>
-				
-				
 
             </div>
+            <?php get_search_form(); ?>
         </nav>
+
     </header>
-
-
-
     <h1>my first wordpress theme</h1>
     <p>iftekher mahmud</p>
 
 
-
-
-
-
-    <?php wp_footer(); ?>
-</body>
-
-</html>
+<?php get_footer(); ?>
